8f4cd70863f458a78ff2405a4fa1c369acfe76f9
mattiacurri <mattia2002@gmail.com>
2023-05-17 21:12:07 +0200

    Implementata la classe CalculatorView

--- View/CalculatorView.java
+++ View/CalculatorView.java
@@ -1,4 +1,34 @@
 package View;
 
+import java.util.Scanner;
+
 public class CalculatorView {
+  public void showLogo() {
+    String logo = """
+          ______     ___       __        ______   ______    __          ___   .___________..______       __    ______  _______\s
+         /      |   /   \\     |  |      /      | /  __  \\  |  |        /   \\  |           ||   _  \\     |  |  /      ||   ____|
+        |  ,----'  /  ^  \\    |  |     |  ,----'|  |  |  | |  |       /  ^  \\ `---|  |----`|  |_)  |    |  | |  ,----'|  |__  \s
+        |  |      /  /_\\  \\   |  |     |  |     |  |  |  | |  |      /  /_\\  \\    |  |     |      /     |  | |  |     |   __| \s
+        |  `----./  _____  \\  |  `----.|  `----.|  `--'  | |  `----./  _____  \\   |  |     |  |\\  \\----.|  | |  `----.|  |____\s
+         \\______/__/     \\__\\ |_______| \\______| \\______/  |_______/__/     \\__\\  |__|     | _| `._____||__|  \\______||_______|
+        """;
+    System.out.println(logo);
+  }
+  public void showMenu() {
+    showLogo();
+    System.out.println("1. Addizione");
+    System.out.println("2. Sottrazione");
+    System.out.println("3. Moltiplicazione");
+    System.out.println("4. Divisione (tra due numeri)");
+  }
+
+  public int getUserChoice() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Inserisci la tua scelta: ");
+    return scanner.nextInt();
+  }
+  public static void main(String[] args) {
+    CalculatorView cv = new CalculatorView();
+    cv.showMenu();
+  }
 }
