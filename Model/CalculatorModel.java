45f52ec27262436d3a922b4f8df0435e40bac167
mattiacurri <mattia2002@gmail.com>
2023-05-17 21:03:00 +0200

    Implementata la classe CalculatorModel

--- Model/CalculatorModel.java
+++ Model/CalculatorModel.java
@@ -1,5 +1,50 @@
 package Model;
 
+import java.math.BigInteger;
+
 public class CalculatorModel {
+  public BigInteger sum(BigInteger... numbers) {
+    BigInteger output = new BigInteger("0");
+    for (BigInteger number : numbers) {
+      output = output.add(number);
+    }
+    return output;
+  }
+
+  public BigInteger subtract(BigInteger... numbers) {
+    BigInteger output = null;
+    try {
+      output = new BigInteger(numbers[0].toByteArray());
+      for (int i = 1; i < numbers.length; i++) {
+        output = output.subtract(numbers[i]);
+      }
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Nessuna operazione da eseguire");
+    }
+    return output;
+  }
+
+  public BigInteger dot(BigInteger... numbers) {
+    BigInteger output = null;
+    try {
+      output = new BigInteger(numbers[0].toByteArray());
+      for (int i = 1; i < numbers.length; i++) {
+        output = output.multiply(numbers[i]);
+      }
+    } catch (ArrayIndexOutOfBoundsException e) {
+      System.out.println("Nessuna operazione da eseguire");
+    }
+    return output;
+  }
+
+  public BigInteger divide(BigInteger numerator, BigInteger denominator) {
+    BigInteger output = new BigInteger("0");
+    try {
+      output = output.add(numerator).divide(denominator);
+    } catch (ArithmeticException e) {
+      System.out.println("Division by zero");
+    }
+    return output;
+  }
 
 }
