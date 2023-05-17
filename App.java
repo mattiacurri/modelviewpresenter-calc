a468cbf5a64962118f3af358477a99833b4af070
mattiacurri <mattia2002@gmail.com>
2023-05-17 20:43:40 +0200

    Creato scheletro

---
+++ App.java
@@ -0,0 +1,11 @@
+import Model.CalculatorModel;
+import View.CalculatorView;
+import Presenter.CalculatorPresenter;
+public class App {
+  public static void main(String[] args) {
+    CalculatorModel calcModel;
+    CalculatorView calcView;
+    CalculatorPresenter calcPresenter;
+  }
+
+}
